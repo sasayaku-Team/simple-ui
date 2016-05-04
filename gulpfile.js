8bededa35d868f6db64f38fc1f736cb83b8fe0b9
kaoru <1404418769@qq.com>
2016-05-04 15:53:17 +0800

    edit gulpfile.js
    👉  add gulp-connect (websevers)
    👉  add gulp-uncss
    👉  edit package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var dirdist = 'dist'
+;
 var gulp = require('gulp'),
 	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
@@ -11,14 +13,41 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch')
 ;
 
-gulp.task('clean', function() {
-  return gulp.src('./' + dirbuild, {
-    read: false
-  })
-  .pipe(clean());
-);
+// gulp.task('clean', function() {
+// 	return gulp.src('./' + dirbuild, {
+// 		read: false
+// 	})
+// 	.pipe(clean());
+// );
 
-gulp.task('default', ['clean'], function() {
-  gulp.start(function() {
-  });
+// gulp.task('default', ['clean'], function() {
+// 	gulp.start(function() {
+// 	});
+// });
+
+// gulp.task('less', function(){
+// 	gulp.src('less/*.less')
+// 		.pipe(less())
+// 		.pipe(gulp.dest('css'))
+// 		.pipe()
+// });
+
+//severs
+gulp.task('connect', function(){
+	connect.server({
+		root: 'dist',
+		port: 5555,
+		livereload: true
+	});
 });
+
+gulp.task('html', function () {
+	gulp.src('./' + dirdist + '/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+	gulp.watch(['./' + dirdist + '/*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
